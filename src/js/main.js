50f5dc67dda84ae1f8d02597f32ed7a7943083f0
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 15:56:18 -0700

    add conditional to check if AJAX response object array length if 0 to display no results message on UI, add form resets on new form submittion, add display message if api response is an error

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,27 @@
 import $ from 'jquery';
 import './../sass/styles.scss';
 import GetDoctor from "./GetDoctor.js";
-
-
 $(document).ready(function() {
   const doctor = new GetDoctor();
   $("#form").submit(function(event) {
+    doctor.resetDoctors();
+    $("#results").empty();
     const medicalIssue = $("#medical-issue").val();
     const doctorName = $("#doctor-name").val();
     event.preventDefault();
     const promise = doctor.getBetterDoctor(doctorName, medicalIssue);
     promise.then(function(response) {
       let body = JSON.parse(response);
-      doctor.createDoctorObjects(body);
-      console.log(doctor.doctors.length);
+      if(body.data.length === 0) {
+        $("#results").append("<h3>We apologize, there are no search results based on your input. Please try again.</h3>");
+      } else {
+        doctor.createDoctorObjects(body);
+      }
       for (var i = 0; i < doctor.doctors.length; i++) {
         $("#results").append(`<div class="doctor"><div><p>First Name: ${doctor.doctors[i].firstName}</p><p>Last Name: ${doctor.doctors[i].lastName}</p><p>${doctor.doctors[0].addressStreet} ${doctor.doctors[i].addressStreet2}</p><p>${doctor.doctors[0].addressCity} ${doctor.doctors[i].addressState}, ${doctor.doctors[i].addressZip}</p><p>Phone: ${doctor.doctors[i].phone}</p><p>Website: ${doctor.doctors[i].website}</p><p>Is currently accepting patients: ${doctor.doctors[i].acceptsNewPatients}</p></div></div>`);
       }
     }, function(error) {
-      console.log(error);
+      $("#results").append(`<h3 id="error-message">${error}</h3>`);
     });
     $("#medical-issue").val("");
     $("#doctor-name").val("");
