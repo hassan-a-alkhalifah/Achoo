e73219681cfa91064bb77aabee843e494e33ba6c
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 11:13:28 -0700

    create a doctor object and promist object, add promise then function which includes console log of AJAX response in JSON format also includes a console log with error messeage if error occurs

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../sass/styles.scss';
+import GetDoctor from "./GetDoctor.js";
 
 
 $(document).ready(function() {
-  
+  const doctor = new GetDoctor();
+  const promise = doctor.getBetterDoctor("Dentist");
+
+  promise.then(function(response) {
+    console.log(response);
+  }, function(error) {
+    console.log(error);
+  });
+
 });
