cd864bbdfa9a4022da4fe4d3186cc39fc7af4283
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 11:59:23 -0700

    add doctor name argument in getBetterDoctor method, add console logs to retrieve required output information for patient based on search input, i.e. firstname, lastname, address, address2, city, state, zip, phone, website and if doctor accepts new patients

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,21 @@ import GetDoctor from "./GetDoctor.js";
 
 $(document).ready(function() {
   const doctor = new GetDoctor();
-  const promise = doctor.getBetterDoctor("Dentist");
+  const promise = doctor.getBetterDoctor("","Dentist");
 
   promise.then(function(response) {
+    let body = JSON.parse(response)
     console.log(response);
+    console.log("First Name: " + body.data[0].profile.first_name);
+    console.log("Last Name: " + body.data[0].profile.last_name);
+    console.log("Address Street: " + body.data[0].practices[0].visit_address.street);
+    console.log("Address Street2: " + body.data[0].practices[0].visit_address.street2);
+    console.log("Address City: " + body.data[0].practices[0].visit_address.city);
+    console.log("Address State: " + body.data[0].practices[0].visit_address.state);
+    console.log("Address Zip: " + body.data[0].practices[0].visit_address.zip);
+    console.log("Phone: " + body.data[0].practices[0].phones[0].number);
+    console.log("Website: " + body.data[0].practices[0].website);
+    console.log("Accepts New Patients: " + body.data[0].practices[0].accepts_new_patients);
   }, function(error) {
     console.log(error);
   });
