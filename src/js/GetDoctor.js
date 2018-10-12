f613c4b49684024ff8de717374fbba22cc6e4700
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 11:11:25 -0700

    add getBetterDoctor method which includes an AJAX request within a new Promise class, url includes query which takes user medical issue input and user_key which has a placeholder for the api key

--- src/js/GetDoctor.js
+++ src/js/GetDoctor.js
@@ -2,4 +2,20 @@ export default class GetDoctor {
   constructor() {
 
   }
+  getBetterDoctor(medicalIssue) {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
 }
