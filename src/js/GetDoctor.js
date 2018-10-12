4002ca364b8caf985841c12c2714e198c9a85182
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 14:29:28 -0700

    add conditional statement to check if each doctor result received has a website or not, if not, add conditional to check if each doctor result accepts new patients or not

--- src/js/GetDoctor.js
+++ src/js/GetDoctor.js
@@ -23,17 +23,24 @@ export default class GetDoctor {
       this.doctors.push({
         "firstName" : response.data[i].profile.first_name,
         "lastName" : response.data[i].profile.last_name,
-        "Address Street" : response.data[i].practices[0].visit_address.street,
-        "Address Street2" : response.data[i].practices[0].visit_address.street2,
-        "Address City" : response.data[i].practices[0].visit_address.city,
-        "Address State" : response.data[i].practices[0].visit_address.state,
-        "Address Zip" : response.data[i].practices[0].visit_address.zip,
-        "Phone" : response.data[i].practices[0].phones[0].number,
-        "Website" : response.data[i].practices[0].website,
-        "Accepts New Patients" : response.data[i].practices[0].accepts_new_patients
+        "addressStreet" : response.data[i].practices[0].visit_address.street,
+        "addressStreet2" : response.data[i].practices[0].visit_address.street2,
+        "addressCity" : response.data[i].practices[0].visit_address.city,
+        "addressState" : response.data[i].practices[0].visit_address.state,
+        "addressZip" : response.data[i].practices[0].visit_address.zip,
+        "phone" : response.data[i].practices[0].phones[0].number,
+        "website" : response.data[i].practices[0].website,
+        "acceptsNewPatients" : response.data[i].practices[0].accepts_new_patients
       });
+      if(this.doctors[i].website === undefined) {
+        this.doctors[i].website = "Not available";
+      }
+      if(this.doctors[i].acceptsNewPatients === true) {
+        this.doctors[i].acceptsNewPatients = "Yes";
+      } else {
+        this.doctors[i].acceptsNewPatients = "No";
+      }
     }
-    console.log(this.doctors);
   }
 
 }
