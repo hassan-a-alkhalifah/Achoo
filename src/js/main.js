d0ef90670ba1db11e27859d2cde82c421e39e889
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 15:24:08 -0700

    add wrapper div around doctor results

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './../sass/styles.scss';
 import GetDoctor from "./GetDoctor.js";
 
@@ -17,7 +15,7 @@ $(document).ready(function() {
       doctor.createDoctorObjects(body);
       console.log(doctor.doctors.length);
       for (var i = 0; i < doctor.doctors.length; i++) {
-        $("#results").append(`<div class="doctor"><p>First Name: ${doctor.doctors[i].firstName}</p><p>Last Name: ${doctor.doctors[i].lastName}</p><p>${doctor.doctors[0].addressStreet} ${doctor.doctors[i].addressStreet2}</p><p>${doctor.doctors[0].addressCity} ${doctor.doctors[i].addressState}, ${doctor.doctors[i].addressZip}</p><p>Phone: ${doctor.doctors[i].phone}</p><p>Website: ${doctor.doctors[i].website}</p><p>Is currently accepting patients: ${doctor.doctors[i].acceptsNewPatients}</p>`);
+        $("#results").append(`<div class="doctor"><div><p>First Name: ${doctor.doctors[i].firstName}</p><p>Last Name: ${doctor.doctors[i].lastName}</p><p>${doctor.doctors[0].addressStreet} ${doctor.doctors[i].addressStreet2}</p><p>${doctor.doctors[0].addressCity} ${doctor.doctors[i].addressState}, ${doctor.doctors[i].addressZip}</p><p>Phone: ${doctor.doctors[i].phone}</p><p>Website: ${doctor.doctors[i].website}</p><p>Is currently accepting patients: ${doctor.doctors[i].acceptsNewPatients}</p></div></div>`);
       }
     }, function(error) {
       console.log(error);
