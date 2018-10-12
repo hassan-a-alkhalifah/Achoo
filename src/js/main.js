ecbae8fde51adf17ad90a8c2e0deba3af643576c
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-12 16:13:18 -0700

    reposition for loop into else statement

--- src/js/main.js
+++ src/js/main.js
@@ -16,9 +16,9 @@ $(document).ready(function() {
         $("#results").append("<h3>We apologize, there are no search results based on your input. Please try again.</h3>");
       } else {
         doctor.createDoctorObjects(body);
-      }
-      for (var i = 0; i < doctor.doctors.length; i++) {
-        $("#results").append(`<div class="doctor"><div><p>First Name: ${doctor.doctors[i].firstName}</p><p>Last Name: ${doctor.doctors[i].lastName}</p><p>${doctor.doctors[0].addressStreet} ${doctor.doctors[i].addressStreet2}</p><p>${doctor.doctors[0].addressCity} ${doctor.doctors[i].addressState}, ${doctor.doctors[i].addressZip}</p><p>Phone: ${doctor.doctors[i].phone}</p><p>Website: ${doctor.doctors[i].website}</p><p>Is currently accepting patients: ${doctor.doctors[i].acceptsNewPatients}</p></div></div>`);
+        for (var i = 0; i < doctor.doctors.length; i++) {
+          $("#results").append(`<div class="doctor"><div><p>First Name: ${doctor.doctors[i].firstName}</p><p>Last Name: ${doctor.doctors[i].lastName}</p><p>${doctor.doctors[0].addressStreet} ${doctor.doctors[i].addressStreet2}</p><p>${doctor.doctors[0].addressCity} ${doctor.doctors[i].addressState}, ${doctor.doctors[i].addressZip}</p><p>Phone: ${doctor.doctors[i].phone}</p><p>Website: ${doctor.doctors[i].website}</p><p>Is currently accepting patients: ${doctor.doctors[i].acceptsNewPatients}</p></div></div>`);
+        }
       }
     }, function(error) {
       $("#results").append(`<h3 id="error-message">${error}</h3>`);
